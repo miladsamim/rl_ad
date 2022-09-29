7a289590fb689a2584f5f54a7d9e5bb814eba46d
ms <msamim18@student.aau.dk>
2022-09-29 14:59:06 +0200

    scenario

---
+++ path.ps1
@@ -0,0 +1,5 @@
+$env:CARLA_ROOT = 'C:\Users\test\Documents\driving\CARLA_0.9.13\WindowsNoEditor'
+$env:SCENARIO_RUNNER_ROOT = 'C:\Users\test\Documents\driving\rl_ad\scenario_runner'
+$env:PYTHONPATH = "$env:Path;$env:CARLA_ROOT\PythonAPI\carla"
+# set PYTHONPATH=%PYTHONPATH%;%CARLA_ROOT%\PythonAPI\carla
+# set PYTHONPATH=%PYTHONPATH%;%CARLA_ROOT%\PythonAPI\carla\dist\carla-0.9.13-py3.7-win-amd64.egg
