1ea1c25446dd9bd76dc114753a7ace4b1b28e987
ms99uni <msamim18@student.aau.dk>
2022-12-11 11:12:51 +0000

    dqn

--- SAC/DQN_Agent/push.sh
+++ SAC/DQN_Agent/push.sh
@@ -1,3 +1,6 @@
 git add models/DriveDQN_simple_fusion_lstm/ -f
+git add models/DriveDQN_cnn_lstm/ -f
+git add models/DriveDQN_simple_fusion/ -f
+git add models/DriveDQN_simple_fusion_single_act_dec/ -f
 git commit -m "dqn data"
 git push
